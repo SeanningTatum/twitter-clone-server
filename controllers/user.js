325d94de9e4d5c2ff8240cee7e5b10f11c519158
Sean Urgel <seantheurgel@gmail.com>
2018-06-21 20:57:03 +0800

    added testing code for createUser

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,19 @@
 
 
 
-/**
+/** 
  * This function creates a user
  * hashes the password then saves 
  * user in database
  * @param {*} req
  */
 exports.createUser = (req, res, next) => {
-
+   console.log(res);
+   const query = `
+      INSERT INTO users (user_ID, name, handle, password) 
+      VALUES ('null', '${res.body.name}', '${res.body.handle}', '${res.body.password}');
+   `
+   res.locals.connection.query(query, (error, results, fields) => {
+      res.send("inserted!");
+   });
 }
