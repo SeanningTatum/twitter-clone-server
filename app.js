a82708dcae63d537c018ffc98dccaf2e50570041
Sean Urgel <seantheurgel@gmail.com>
2018-06-21 20:36:51 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+// Routes
+const index = require ('./routes/index');
+
+// Database
+app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'twitter-remake'
+  });
+
+  res.locals.connection.connect();
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(logger('dev'));
+
+// configure routes
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // send the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+// GET Tweets
+router.get('/', (req, res, next) => {
+  const query = 'SELECT * from tweets';
+
+  res.locals.connection.query(query, (error, results, fields) => {
+    if (error) return res.status(500);
+    
+    res.status(200);
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+
+/**
+ * POST method for ('/');
+ * @param {message: string}
+ */
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+  const query = `
+      INSERT INTO tweets (name, handlerName, message, date, tweet_ID) 
+      VALUES ('test', 'test', '${req.body.message}', '${date}', NULL)
+    `;
+  
+  res.locals.connection.query(query, (error, results, fields) => {
+    if (error) return res.status(500);
+    
+    res.status(200);
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	})
+  
+});
+
+module.exports = router;
