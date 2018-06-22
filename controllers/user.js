9e71afd2f700beecc05f4fc54621c405c19e0159
Sean Urgel <seantheurgel@gmail.com>
2018-06-22 11:07:47 +0800

    installed bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,6 @@ const mysql = require("mysql");
  * This function creates a user
  * hashes the password then saves 
  * user in database
- * @param {*} req
  */
 exports.createUser = (req, res, next) => {
    console.log(req.body);
